764a2bf40f5aac5e40e52f914358603a089035e4
hassaanmgl8844 <hsnprog8844@gmail.com>
2024-11-19 21:35:22 +0500

    Working on Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,18 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-        This is what they called Navbar
+    <div className='h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'>
+        {/* LOGO */}
+        <div className="">
+            <Link href={"/"} className='text-sm bg-black rounded-md p-1 font-semibold'>
+                <span className='text-white'>Hassaam</span>
+                <span className='w-12 h-8 rounded bg-white text-black'>.dev</span>
+            </Link>
+        </div>
+        {/* RESPONSIVE MENU  */}
+        <div className="">2</div>
     </div>
   )
 }
