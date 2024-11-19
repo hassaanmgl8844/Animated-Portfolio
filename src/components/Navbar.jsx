7169dc4aaddd3bb5d05b8b953c61806ee33580bb
hassaanmgl8844 <hsnprog8844@gmail.com>
2024-11-19 21:37:31 +0500

    Logo is Finished in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,9 +6,9 @@ const Navbar = () => {
     <div className='h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'>
         {/* LOGO */}
         <div className="">
-            <Link href={"/"} className='text-sm bg-black rounded-md p-1 font-semibold'>
-                <span className='text-white'>Hassaam</span>
-                <span className='w-12 h-8 rounded bg-white text-black'>.dev</span>
+            <Link href={"/"} className='text-sm bg-black rounded-md p-1 font-semibold flex  items-center justify-center'>
+                <span className='text-white mr-1'>Hassaam</span>
+                <span className='w-12 h-8 rounded bg-white text-black flex items-center justify-center'>.dev</span>
             </Link>
         </div>
         {/* RESPONSIVE MENU  */}
