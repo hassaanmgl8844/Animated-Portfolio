c1dd7b884898d8549415014ceb645e808fd374ad
hassaanmgl8844 <hsnprog8844@gmail.com>
2024-11-20 10:25:31 +0500

    Working on Navbar as Started working on PC version

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,8 +14,15 @@ const Navbar = () => {
 
   return (
     <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+      <div className="hidden md:flex  gap-4">
+        {links.map(link =>(
+          <Link href={link.url} key={link.title}>
+            {link.title}
+          </Link>
+        ))}
+      </div>
       {/* LOGO */}
-      <div className="">
+      <div className="md:hidden">
         <Link
           href={"/"}
           className="text-sm bg-black rounded-md p-1 font-semibold flex  items-center justify-center"
@@ -27,7 +34,7 @@ const Navbar = () => {
         </Link>
       </div>
       {/* RESPONSIVE MENU  */}
-      <div className="">
+      <div className="md:hidden">
         {/* MENU BUTTON */}
         <button
           className="w-10 h-8 flex flex-col justify-between z-50 relative"
