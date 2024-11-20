0d29484d7de975d979f5c28b298cac93e4bb0d2c
hassaanmgl8844 <hsnprog8844@gmail.com>
2024-11-20 21:01:27 +0500

    Added some feature in transtion

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,10 +1,18 @@
+"use client"
+
 import React from 'react'
+import { motion } from 'framer-motion'
 
 const PortfolioPage = () => {
   return (
-    <div>
+    <motion.div
+    className="h-full"
+    initial={{ y: "-200vh" }}
+    animate={{ y: "0%" }}
+    transition={{ duration: 1 }}
+  >
       Portfolio      
-    </div>
+    </motion.div>
   )
 }
 
