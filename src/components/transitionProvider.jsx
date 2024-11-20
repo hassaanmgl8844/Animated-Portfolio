1c55395c0581663d117d3dad32cf633959ec7b32
hassaanmgl8844 <hsnprog8844@gmail.com>
2024-11-20 15:54:12 +0500

    Working on A Page Changing Transition

--- src/components/transitionProvider.jsx
+++ src/components/transitionProvider.jsx
@@ -7,17 +7,33 @@ import { motion } from "framer-motion";
 import { usePathname } from "next/navigation";
 
 const TransitionProvider = ({ children }) => {
-
-    const pathName = usePathname()
+  const pathName = usePathname();
 
   return (
-    <AnimatePresence>
-      <div key={pathName} className="w-screen h-screen bg-gradient-to-b from-blue-100 to-red-100">
+    <AnimatePresence mode="wait">
+      <div
+        key={pathName}
+        className="w-screen h-screen bg-gradient-to-b from-blue-100 to-red-100"
+      >
         <motion.div
           className="h-screen w-screen fixed bg-black rounded-b-[100px] z-40"
           animate={{ height: "0vh" }}
-          exit={{height:"100vh"}}
-          transition={{duration:0.5, ease:"easeOut"}}
+          exit={{ height: "140vh" }}
+          transition={{ duration: 2, ease: "easeOut" }}
+        />
+        <motion.div
+          className="fixed m-auto top-0 bottom-0 left-0 right-0 text-white text-8xl cursor-default z-50 w-fit h-fit"
+          initial={{ opacity: 1 }}
+          animate={{ opacity: 0 }}
+          exit={{ opacity: 0 }} 
+          transition={{ duration: 2, ease: "easeOut" }}
+        >
+            {pathName}
+        </motion.div>
+        <motion.div
+          className="h-screen w-screen fixed bg-black rounded-t-[100px] bottom-0 z-40"
+          initial={{ height: "140vh" }}
+          animate={{ height: "0vh", transition: { delay: 2 } }}
         />
         <div className="h-24">
           <Navbar />
