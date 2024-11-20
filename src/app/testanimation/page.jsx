495e9e164ee1e28b14f6d3c2ea448a9b89625c82
hassaanmgl8844 <hsnprog8844@gmail.com>
2024-11-20 14:37:26 +0500

    three lines to cross animation done

--- src/app/testanimation/page.jsx
+++ src/app/testanimation/page.jsx
@@ -4,13 +4,29 @@ import React from "react";
 import { motion } from "framer-motion";
 
 const Testpage = () => {
+  const variants = {
+    variant1: {
+      x: 400,
+      y: 300,
+      opacity: 0.5,
+      transition: {
+        duration: 3,
+
+      }
+    },
+    variant2: {
+      x: 100,
+      y: -300,
+      rotation: 90,
+    },
+  };
+
   return (
     <div className="flex items-center justify-center ">
       <motion.div
         className="w-96 h-96 bg-red-400 rounded"
-        initial={{ x: -100 }}
-        animate={{ x: 100 , y: 300 , opacity: 0.5 }}
-        transition={{delay:2,duration:4}}
+        variants={variants}
+        animate="variant1"
       ></motion.div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
 import NavLink from "./navLink";
+import { motion } from "framer-motion";
 
 const links = [
   { url: "/", title: "Home" },
@@ -13,6 +14,35 @@ const links = [
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
+
+  const topVariants = {
+    closed: {
+      rotate: 0,
+    },
+    opened: {
+      rotate: 45,
+      backgroundColor: "rgba(255,255,255)",
+    },
+  };
+
+  const centerVariants = {
+    closed: {
+      opacity: 1,
+    },
+    opened: {
+      opacity: 0,
+    },
+  };
+
+  const bottomVariants = {
+    closed: {
+      rotate: 0,
+    },
+    opened: {
+      rotate: -45,
+      backgroundColor: "rgba(255,255,255)",
+    },
+  };
 
   return (
     <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
@@ -42,7 +72,10 @@ const Navbar = () => {
         <Link href={"https://github.com/hassaammgl"} target="_blank">
           <Image src="/instagram.png" alt="github" width={24} height={24} />
         </Link>
-        <Link href={"https://www.linkedin.com/in/m-hassaam-mughal-91668a256/"} target="_blank">
+        <Link
+          href={"https://www.linkedin.com/in/m-hassaam-mughal-91668a256/"}
+          target="_blank"
+        >
           <Image src="/linkedin.png" alt="github" width={24} height={24} />
         </Link>
       </div>
@@ -53,9 +86,21 @@ const Navbar = () => {
           className="w-10 h-8 flex flex-col justify-between z-50 relative"
           onClick={() => setOpen((prev) => !prev)}
         >
-          <div className="w-10 h-1  bg-white rounded"></div>
-          <div className="w-10 h-1  bg-white rounded"></div>
-          <div className="w-10 h-1  bg-white rounded"></div>
+          <motion.div
+            variants={topVariants}
+            animate={open ? "opened": "closed"}
+            className="w-10 h-1  bg-black rounded origin-left"
+          ></motion.div>
+          <motion.div
+            variants={centerVariants}
+            animate={open ? "opened": "closed"}
+            className="w-10 h-1  bg-black rounded"
+          ></motion.div>
+          <motion.div
+            variants={bottomVariants}
+            animate={open ? "opened": "closed"}
+            className="w-10 h-1  bg-black rounded origin-left"
+          ></motion.div>
         </button>
         {/* MENU LIST  */}
         {open && (
