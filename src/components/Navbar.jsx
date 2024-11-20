74bb201ce60bfb0e3b34accdf4c8d489ef5f41e2
hassaanmgl8844 <hsnprog8844@gmail.com>
2024-11-20 10:46:07 +0500

    Error Solved

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
+import NavLink from "./navLink";
 
 const links = [
   { url: "/", title: "Home" },
@@ -15,15 +16,17 @@ const Navbar = () => {
 
   return (
     <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
-      <div className="hidden md:flex  gap-4">
+      {/* LINKS  */}
+      <div className="hidden md:flex gap-4 w-1/3">
         {links.map((link) => (
-          <Link href={link.url} key={link.title}>
-            {link.title}
-          </Link>
+          <NavLink link={link} key={link.title} />
+          // <Link href={link.url} key={link.title}>
+          //   {link.title}
+          // </Link>
         ))}
       </div>
       {/* LOGO */}
-      <div className="md:hidden">
+      <div className="md:hidden lg:flex w-1/3 justify-center">
         <Link
           href={"/"}
           className="text-sm bg-black rounded-md p-1 font-semibold flex  items-center justify-center"
@@ -34,7 +37,8 @@ const Navbar = () => {
           </span>
         </Link>
       </div>
-      <div className="">
+      {/* SOCIAL  */}
+      <div className="hidden md:flex gap-4 w-1/3">
         <Link href={"https://github.com/hassaammgl"} target="_blank">
           <Image src="/github.png" alt="github" width={24} height={24} />
         </Link>
